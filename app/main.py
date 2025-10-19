b89933033a0aba55dbefef01502feabdb57e32f7
lakshith-403 <lakshith.k.nishshanke@gmail.com>
2025-10-19 15:00:37 +0530

    feat: add localhost to cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3001",
-        "http://127.0.0.1:3001"
+        "http://127.0.0.1:3001",
+        "http://localhost",
     ],
     allow_credentials=True,
     allow_methods=["*"],
